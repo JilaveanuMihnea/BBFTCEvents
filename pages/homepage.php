d87c25e7335478a75bb6a678a21837e14a27b3be
Maritrioska <marionescu2000@gmail.com>
2023-10-30 16:22:18 +0200

    sidebar in principiu

---
+++ pages/homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="../style/sidebar.css">
+</head>
+
+<body>
+  <label>
+    <input type="checkbox">
+    <div class="toggle">
+      <span class="top_line common"></span>
+      <span class="middle_line common"></span>
+      <span class="bottom_line common"></span>
+    </div>
+    <div class="slide">
+      <h1>MENU</h1>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Add event</a></li>
+      </ul>
+    </div>
+  </label>
+</body>
+
+</html>
