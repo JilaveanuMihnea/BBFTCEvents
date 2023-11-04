1d2eee168b192d9b446dae736c640b7164db609b
Jilaveanu Mihnea <jilaveanu.mihnea@gmail.com>
2023-11-04 13:13:51 +0200

    efinder stlu

--- js/eventfilter.js
+++ js/eventfilter.js
@@ -1,24 +1,37 @@
 // todo fill in dropdown options
 
-const selectBtns = document.querySelectorAll(".select-btn");
-const items = document.querySelectorAll(".selection");
+const showfilters = document.querySelector('#show-filters');
+const popup = document.querySelector('.popup-form');
+const popupobf = document.querySelector('#popup-obfuscate');
+const selectBtns = document.querySelectorAll('.select-btn');
+const items = document.querySelectorAll('.selection');
+
+showfilters.addEventListener('click', () => {
+  popup.classList.add('active');
+  popupobf.classList.add('active');
+});
+
+popupobf.addEventListener('click', () => {
+  popup.classList.remove('active');
+  popupobf.classList.remove('active');
+});
 
 selectBtns.forEach((selectBtn) => {
-  selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
+  selectBtn.addEventListener('click', () => {
+    selectBtn.classList.toggle('open');
   });
 });
 
 items.forEach((item) => {
-  item.addEventListener("change", () => {
-    item.classList.toggle("selected");
+  item.addEventListener('change', () => {
+    item.classList.toggle('selected');
 
-    let checked = document.querySelectorAll(".selected");
-    let btnText = document.querySelector(".btn-text");
+    let checked = document.querySelectorAll('.selected');
+    let btnText = document.querySelector('.btn-text');
     if (checked && checked.length > 0) {
-      btnText.innerText = checked.length + " selectate";
+      btnText.innerText = checked.length + ' selectate';
     } else {
-      btnText.innerText = "Selecteaza judete";
+      btnText.innerText = 'Selecteaza judete';
     }
   });
 });
